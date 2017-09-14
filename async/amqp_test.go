fb9d23978f10edc842e035f4734ca68f91a01bb8
Ricardo Gamba <rgamba@gmail.com>
2017-09-13 22:37:18 -0500

    More tests

--- async/amqp_test.go
+++ async/amqp_test.go
@@ -1,13 +1,16 @@
 package async
 
 import (
+	"io/ioutil"
 	"os"
 	"testing"
 
+	log "github.com/sirupsen/logrus"
 	"github.com/streadway/amqp"
 )
 
 func TestMain(m *testing.M) {
+	log.SetOutput(ioutil.Discard)
 	os.Exit(m.Run())
 }
 
@@ -70,6 +73,55 @@ func TestConsumeFromResponseQueue(t *testing.T) {
 	<-c
 }
 
+func TestCacheSendChannel(t *testing.T) {
+	_connect()
+	cacheSendChannel("test", nil)
+	ch, ok := sendChannels["test"]
+	if !ok {
+		t.Errorf("Unable to get 'test' sendChannel")
+	}
+	if ch != nil {
+		t.Errorf("Inconrrect channel value")
+	}
+}
+
+func TestDeleteSendChannelCache(t *testing.T) {
+	_connect()
+	sendChannels["test"] = nil
+	deleteSendChannelCache("test")
+	_, ok := sendChannels["test"]
+	if ok {
+		t.Errorf("Unable to delete 'test' sendChannel")
+	}
+}
+
+func TestQueueExists(t *testing.T) {
+	_connect()
+	ch, _ := conn.Channel()
+	if exists := queueExists(ch, getRequestQueueName()); !exists {
+		t.Errorf("Unexpected value for queueExists")
+	}
+}
+
+func TestPublishMessage(t *testing.T) {
+	_connect()
+	resp := make(chan []byte)
+	_createQueue("test")
+	_consumeQueue("test", func(msg []byte) {
+		resp <- msg
+	})
+	ch, _ := conn.Channel()
+	err := publishMessage(ch, []byte("test"), "test")
+	if err != nil {
+		t.Error("Unexpected error: ", err.Error())
+	}
+	if string(<-resp) != "test" {
+		t.Error("Unexpected response")
+	}
+}
+
+// Misc functions
+
 func _queueExists(queueName string) bool {
 	ch, _ := conn.Channel()
 	_, err := ch.QueueInspect(queueName)
@@ -97,4 +149,33 @@ func _publishMessage(message []byte, queueName string) error {
 		},
 	)
 	return err
+}
+
+func _consumeQueue(queueName string, fn func([]byte)) {
+	msgs, _ := responseChannel.Consume(
+		queueName, // Queue name
+		"",        // Consumer
+		true,      // Auto ack
+		false,     // Exclusive
+		false,     // No-local
+		false,     // No-wait
+		nil,       // args
+	)
+	go func() {
+		for d := range msgs {
+			fn(d.Body)
+		}
+	}()
+}
+
+func _createQueue(name string) {
+	ch, _ := conn.Channel()
+	ch.QueueDeclare(
+		name,  // Name
+		true,  // Durable
+		true,  // Delete when unused
+		false, // Exclusive
+		false, // No-wait
+		nil,   // arguments
+	)
 }
